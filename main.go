1e251e168265732bc3bc276d8d894a25b7909f01
Tevz <tevz.8bajzelj@gmail.com>
2024-03-18 21:01:38 +0100

    Add template example

--- main.go
+++ main.go
@@ -3,14 +3,32 @@ package main
 import (
     "os"
     "fmt"
-    "filepath"
+    "path/filepath"
+    "html/template"
 )
 
-var CurrentDir := "/dev/test/spg"
+var CurrentDir string = "/dev/test/spg"
 
-func main() {
-    dirs, _ := os.ReadDir(filepath.Join(CurrentDir, "content");
+func read_dirs() {
+    dirs, _ := os.ReadDir(filepath.Join(CurrentDir, "content"))
     for i, j := range dirs {
         fmt.Printf("%d, %#v\n", i, j.Name());
+    }
+}
+
+type HtmlDataIndex struct {
+    Title string
+    Menus []string
+}
+
+func main() {
+    t, err := template.ParseFiles(filepath.Join(CurrentDir, "layout/index.html"))
+    if err != nil {
+        panic(err)
+    }
+    data := HtmlDataIndex { "title 1", []string{"Index", "Content", "Flags"} }
+    err = t.Execute(os.Stdout, data)
+    if err != nil {
+        panic(err)
     }
 }
